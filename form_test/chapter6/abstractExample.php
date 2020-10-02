b76a39618b5784d003e117dc25c1cf517138a480
Joseph-zhang-dot <xzhang13@wccnet.edu>
2020-10-02 15:12:56 -0400

    commit

---
+++ form_test/chapter6/abstractExample.php
@@ -0,0 +1,18 @@
+<?php
+abstract class car{
+    public $name;
+    public function _construct($name){
+        $this->name = $name;
+    }
+    abstract public function intro() : string;
+}
+
+class Audi extends car{
+    public function intro() : string{
+        return"Choose German quality! I'm an $this->name!";
+    }
+}
+$audi = new audi("Audi");
+echo $audo->intro;
+echo "<br>";
+?>
