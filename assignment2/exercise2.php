6263169f66289d8ccb6e139622aaa8d387d4e798
joseph <xzhang13@wccnet.edu>
2020-10-09 11:29:47 -0400

    assignment2

--- assignment2/exercise2.php
+++ assignment2/exercise2.php
@@ -1,56 +1,57 @@
+
+<?php
+
+$text="Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+Vivamus feugiat mollis dolor at bibendum. In congue maximus 
+ligula, ut faucibus mi accumsan at. Vestibulum sagittis tortor
+eget dui ultricies, a vulputate lacus faucibus. Fusce aliquet
+bibendum erat, sed bibendum eros cursus eu. Nulla at neque
+	rhoncus, ultricies odio at, accumsan elit. Proin in turpis
+	eu leo dapibus pulvinar. Vivamus viverra massa ut enim 
+	fringilla ultricies. Donec in enim blandit, iaculis nulla
+	quis, egestas elit. Nullam ut enim id erat bibendum finibus 
+	nec ac eros. Nulla malesuada ex facilisis ultrices rhoncus. 
+	Nullam in euismod nisl. Donec pulvinar ex sit amet aliquet
+	egestas.";
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<title>&lt;My Web Page&gt;</title>
-	<style>
-		* {margin: 0; padding: 0;}
-		body {font: 120%/1.5 sans-serif;}
-		#wrapper {width: 1000px; margin: 0 auto; border: 1px solid black;}
-		header {background: green; height: 150px; padding: 20px;}
-		header h1 {color: white;}
-		main {padding: 10px;}
-		main h2 {margin: 15px 0;}
-		main p {margin-bottom: 15px;}
-		footer {background: #eee; padding: 10px 0; text-align: center}
-		footer p {font-size: .8em;}
-	</style>
+<meta charset="UTF-8">
+<title>&lt;My Web Page&gt;</title>
+<style>
+	* {margin: 0; padding: 0;}
+	body {font: 120%/1.5 sans-serif;}
+	#wrapper {width: 1000px; margin: 0 auto; border: 1px solid black;}
+	header {background: green; height: 150px; padding: 20px;}
+	header h1 {color: white;}
+	main {padding: 10px;}
+	main h2 {margin: 15px 0;}
+	main p {margin-bottom: 15px;}
+	footer {background: #eee; padding: 10px 0; text-align: center}
+	footer p {font-size: .8em;}
+</style>
 </head>
 <body>
-	<div id="wrapper">
-		<header>
-			<h1>My Web Page</h1>
-		</header>
+<div id="wrapper">
+	<header>
+		<h1>My Web Page</h1>
+	</header>
+	<main>
+	<h2>My name is Scott Shaper</h2>
 
+	<?php 
+	for ($i = 0; $i <3; $i++){
+		echo $text;
+		echo "<br>";
+	}
+	?>
 		
-		<main>
-		<h2>My name is Scott Shaper</h2>
+	</main>
+	<footer>
+		<p>My Web Page @2020</p>
+	</footer>
+</div>
 
-		<?php
-
-		$text="Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-		Vivamus feugiat mollis dolor at bibendum. In congue maximus 
-		ligula, ut faucibus mi accumsan at. Vestibulum sagittis tortor
-		eget dui ultricies, a vulputate lacus faucibus. Fusce aliquet
-		bibendum erat, sed bibendum eros cursus eu. Nulla at neque
-			rhoncus, ultricies odio at, accumsan elit. Proin in turpis
-			eu leo dapibus pulvinar. Vivamus viverra massa ut enim 
-			fringilla ultricies. Donec in enim blandit, iaculis nulla
-			quis, egestas elit. Nullam ut enim id erat bibendum finibus 
-			nec ac eros. Nulla malesuada ex facilisis ultrices rhoncus. 
-			Nullam in euismod nisl. Donec pulvinar ex sit amet aliquet
-			egestas.";
-		for ($i = 0; $i <3; $i++){
-			echo $text;
-			echo "<br>";
-		}
-		?>
-			
-		</main>
-		<footer>
-			<p>My Web Page @2020</p>
-		</footer>
-	</div>
-	
 </body>
 </html>
