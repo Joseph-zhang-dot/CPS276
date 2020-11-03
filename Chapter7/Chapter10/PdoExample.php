be8a800f4b45b53f597b5561b7b20cd55798ffc2
joseph <xzhang13@wccnet.edu>
2020-11-02 21:26:47 -0500

    some

---
+++ Chapter7/Chapter10/PdoExample.php
@@ -0,0 +1,22 @@
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+try{
+    $conn = new PDO("mysql: host = $servername; dbname = myDB", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "CREATE TABLE MyGuests(
+        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        firstname VARCHAR(30) NOT NULL,
+        lastname VARCHAR(30) NOT NULL,
+        reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    )";
+    $conn->exec($sql);
+    echo "Connected successfully";
+}catch(PDOException $e){
+    echo "Connection failed: " . $e->getMessage(); 
+}
+
+$conn = null;
+?>
